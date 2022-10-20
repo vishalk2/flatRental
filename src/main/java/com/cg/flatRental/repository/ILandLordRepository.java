867ee414a62ba2a237b5392d94405c4f79840e99
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-10-20 09:29:02 +0530

    Update ILandLordRepository.java

--- src/main/java/com/cg/flatRental/repository/ILandLordRepository.java
+++ src/main/java/com/cg/flatRental/repository/ILandLordRepository.java
@@ -1,14 +1,11 @@
 package com.cg.flatRental.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.cg.flatRental.entity.BookingRequest;
 import com.cg.flatRental.entity.LandLord;
 
 @Repository
