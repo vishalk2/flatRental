ee66b2ebce2d2314ad228f5243fcf18655595db7
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-10-20 09:27:28 +0530

    Add files via upload

---
+++ src/main/java/com/cg/flatRental/repository/IAdminRepository.java
@@ -0,0 +1,11 @@
+package com.cg.flatRental.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.flatRental.entity.Admin;
+
+@Repository
+public interface IAdminRepository extends JpaRepository<Admin, Integer> {
+	// CRUD Operations
+}
