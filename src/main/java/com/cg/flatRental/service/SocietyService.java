ce0c3e5b228bf373c51f1e87a934eb9d95a7a1b5
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-10-21 01:36:27 +0530

    Update SocietyService.java

--- src/main/java/com/cg/flatRental/service/SocietyService.java
+++ src/main/java/com/cg/flatRental/service/SocietyService.java
@@ -5,20 +5,30 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.cg.flatRental.dto.SocietyDto;
+import com.cg.flatRental.entity.Address;
+import com.cg.flatRental.entity.LandLord;
 import com.cg.flatRental.entity.Society;
 import com.cg.flatRental.exceptions.SocietyNotFoundException;
+import com.cg.flatRental.iservice.ISocietyService;
+import com.cg.flatRental.repository.ILandLordRepository;
 import com.cg.flatRental.repository.ISocietyRepository;
 
-@Component
+@Service
 public class SocietyService implements ISocietyService {
 	
 	@Autowired
 	private ISocietyRepository societyRepository;
+	
+	@Autowired
+	private ILandLordRepository landlordRepository;
 
 	@Override
-	public Society addSocietyService(Society society) {
+	public Society addSocietyService(SocietyDto societyDto) throws SocietyNotFoundException {
+		Society society = converterToDtoFromEntity(societyDto);
 		Society newSociety = societyRepository.save(society);
 		return newSociety;
 	}
@@ -32,7 +42,7 @@ public class SocietyService implements ISocietyService {
 			return updatedSociety;
 		}
 		else {
-			throw new SocietyNotFoundException("Society details not found!");
+			 throw new SocietyNotFoundException("Society details not found!");
 		}
 	}
 
@@ -45,7 +55,7 @@ public class SocietyService implements ISocietyService {
 			return society;
 		}
 		else {
-			throw new SocietyNotFoundException("Society details not found!");
+			 throw new SocietyNotFoundException("Society details not found!");
 		}
 	}
 
@@ -65,7 +75,7 @@ public class SocietyService implements ISocietyService {
 			return society;
 		}
 		else {
-			throw new SocietyNotFoundException("Society details not found!");
+			 throw new SocietyNotFoundException("Society details not found!");
 		}
 	}
 
@@ -104,5 +114,67 @@ public class SocietyService implements ISocietyService {
 		List<Society> societiesList = societyRepository.filterBySocietyPincode(pincode);
 		return societiesList;
 	}
-}
 
+	@Override
+	@Transactional
+	public Society updateSocietyApprovalService(int societyId, boolean approved) throws SocietyNotFoundException {
+		int s = societyRepository.updateSocietyApproval(societyId, approved);
+		if(s>0) {
+			return getSocietyServiceById(societyId);
+		}
+		else {
+			 throw new SocietyNotFoundException("Society could not be approved");
+		}
+	}
+	
+	public Society converterToDtoFromEntity(SocietyDto societyDto) throws SocietyNotFoundException {
+		long landlordId = societyDto.getLandlordId();
+		Optional<LandLord> optionalLandlord = landlordRepository.findById(landlordId);
+		if(optionalLandlord.isPresent()) {
+			
+			LandLord landlord = optionalLandlord.get();
+			Address societyAddress = new Address();
+			societyAddress.setArea(societyDto.getArea());
+			societyAddress.setCity(societyDto.getCity());
+			societyAddress.setState(societyDto.getState());
+			societyAddress.setCountry(societyDto.getCountry());
+			societyAddress.setPinCode(societyDto.getPincode());
+			
+			Society society = new Society();
+			society.setApproved(false);
+			society.setSocietyName(societyDto.getSocietyName());
+			society.setSocietyLandlord(landlord);
+			society.setSocietyAddress(societyAddress);
+			
+			return society;
+		}
+		else {
+			throw new SocietyNotFoundException("Landlord details could not be found!");
+		}
+	}
+
+	@Override
+	@Transactional
+	public Society updateSocietyAddressService(int societyId, Address address) throws SocietyNotFoundException {
+		int s = societyRepository.updateSocietyAddress(societyId, address.getArea(), address.getCity(), 
+				address.getState(), address.getCountry(), address.getPinCode());
+		if(s>0) {
+			return getSocietyServiceById(societyId);
+		}
+		else {
+			 throw new SocietyNotFoundException("Society detials could not be found");
+		}
+	}
+
+	@Override
+	@Transactional
+	public Society updateSocietyName(int societyId, String societyName) throws SocietyNotFoundException {
+		int s = societyRepository.updateSocietyName(societyId, societyName);
+		if(s>0) {
+			return getSocietyServiceById(societyId);
+		}
+		else {
+			 throw new SocietyNotFoundException("Society detials could not be found");
+		}
+	}
+}
