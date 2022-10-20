6e683f2d743316283d8e070700ed7c905ed5ba61
preetham1243 <72698235+preetham1243@users.noreply.github.com>
2022-10-20 14:17:17 +0530

    updated IUserRepository

--- src/main/java/com/cg/flatRental/repository/IUserRepository.java
+++ src/main/java/com/cg/flatRental/repository/IUserRepository.java
@@ -1,26 +1,12 @@
 package com.cg.flatRental.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-import com.cg.flatRental.entity.User;
+import com.cg.flatRental.secure.User;
 
-@Repository
-public interface IUserRepository extends JpaRepository<User, Integer> {
+
+public interface IUserRepository extends JpaRepository<User, Integer>{
+
+	User findByUserName(String userName);
 	
-	// find user by user name and password
-	public User findByUserNameAndPassword(String userName, String password);
-	
-	// find all users by user type
-	public List<User> findByUserType(String userType);
-	
-	// update user password
-	@Query("update User u set u.password = :pwd where u.userId= :uid")
-	@Modifying
-	public int updateUserPassword(@Param("uid") int userId, @Param("pwd") String newPassword);
 }
