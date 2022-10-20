f28f9d60dfa4ed5e946dbae444936152c07986e6
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-10-20 09:22:29 +0530

    Add files via upload

---
+++ src/main/java/com/cg/flatRental/exceptions/AdminNotFoundException.java
@@ -0,0 +1,11 @@
+package com.cg.flatRental.exceptions;
+
+public class AdminNotFoundException extends Exception {
+	
+	public AdminNotFoundException() {
+	}
+	
+	public AdminNotFoundException(String msg) {
+		super(msg);
+	}
+}
