0bf1e45bfb7d20ed8c1c2bcf69d250fa4e444d35
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-10-21 01:32:21 +0530

    Update ISocietyRepository.java

--- src/main/java/com/cg/flatRental/repository/ISocietyRepository.java
+++ src/main/java/com/cg/flatRental/repository/ISocietyRepository.java
@@ -3,12 +3,33 @@ package com.cg.flatRental.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.cg.flatRental.entity.Society;
 
 public interface ISocietyRepository extends JpaRepository<Society, Integer>{
+	
+	// update society approval
+	@Query("update Society s set s.approved = :app where s.societyId = :sid")
+	@Modifying
+	public int updateSocietyApproval(@Param("sid") int societyId, @Param("app") boolean approved);
+	
+	// update society approval
+	@Query("update Society s set s.societyName = :app where s.societyId = :sid")
+	@Modifying
+	public int updateSocietyName(@Param("sid") int societyId, @Param("app") String societyName);
+	
+	// update society address
+	@Query("update Society s set s.societyAddress.area = :area, "
+			+ "s.societyAddress.city = :city, s.societyAddress.state = :state, "
+			+ "s.societyAddress.country = :country, "
+			+ "s.societyAddress.pincode = :pin where s.societyId = :sid")
+	@Modifying
+	public int updateSocietyAddress(@Param("sid") int societyId, @Param("area") String area,
+			@Param("city") String city, @Param("state") String state, 
+			@Param("country") String country, @Param("pin") int pincode);
 	
 	// find societies by their name
 	public List<Society> findBySocietyName(String societyName);
