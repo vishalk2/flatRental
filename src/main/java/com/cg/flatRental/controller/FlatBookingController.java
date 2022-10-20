5c9f18b6d42984ae986b6e81c2296df5a3d669d7
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-10-21 01:18:29 +0530

    Update FlatBookingController.java

--- src/main/java/com/cg/flatRental/controller/FlatBookingController.java
+++ src/main/java/com/cg/flatRental/controller/FlatBookingController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cg.flatRental.dto.FlatBookingDto;
 import com.cg.flatRental.entity.FlatBooking;
-import com.cg.flatRental.exceptions.FlatBookingNotFoundException;
-import com.cg.flatRental.service.IFlatBookingService;
+import com.cg.flatRental.exceptions.FlatBookingException;
+import com.cg.flatRental.iservice.IFlatBookingService;
 
 @RestController
 @RequestMapping("/flatbooking")
@@ -26,28 +28,32 @@ public class FlatBookingController {
 	private IFlatBookingService flatbookingService;
 	
 	@PostMapping(produces= {"application/json","application/xml"},consumes= {"application/json","application/xml"})
-	public ResponseEntity<FlatBooking> addFlatBooking(@RequestBody FlatBooking flatBooking){
-		return new ResponseEntity<>(flatbookingService.addFlatBookingService(flatBooking),HttpStatus.OK);
+	@PreAuthorize("hasAuthority('admin')")
+	public ResponseEntity<FlatBooking> addFlatBooking(@RequestBody FlatBookingDto flatBookingDto) throws FlatBookingException{
+		return new ResponseEntity<>(flatbookingService.addFlatBookingService(flatBookingDto),HttpStatus.OK);
 	}
 	
 	@PutMapping(value="/{bookingId}",produces= {"application/json","application/xml"},consumes= {"application/json","application/xml"})
-	public ResponseEntity<FlatBooking> updateFlatBooking(@PathVariable int bookingId, FlatBooking flatBooking) throws FlatBookingNotFoundException{
+	@PreAuthorize("hasAuthority('admin')")
+	public ResponseEntity<FlatBooking> updateFlatBooking(@PathVariable int bookingId, FlatBooking flatBooking) throws FlatBookingException{
 		return new ResponseEntity<>(flatbookingService.updateFlatBookingService(bookingId, flatBooking),HttpStatus.OK);
 	}
 	
 	@GetMapping(value="/{bookingId}",produces= {"application/json","application/xml"},consumes= {"application/json","application/xml"})
-	public ResponseEntity<FlatBooking> getFlatBooking(@PathVariable int bookingId) throws FlatBookingNotFoundException{
+	@PreAuthorize("hasAuthority('admin')")
+	public ResponseEntity<FlatBooking> getFlatBooking(@PathVariable int bookingId) throws FlatBookingException{
 		return new ResponseEntity<>(flatbookingService.viewFlatBookingService(bookingId),HttpStatus.OK);
 	}
 	
 	@DeleteMapping(value="/{bookingId}",produces= {"application/json","application/xml"},consumes= {"application/json","application/xml"})
-	public ResponseEntity<FlatBooking> removeFlatBooking(@PathVariable int bookingId) throws FlatBookingNotFoundException{
+	@PreAuthorize("hasAuthority('admin')")
+	public ResponseEntity<FlatBooking> removeFlatBooking(@PathVariable int bookingId) throws FlatBookingException{
 		return new ResponseEntity<>(flatbookingService.deleteFlatBookingService(bookingId),HttpStatus.OK);
 	}
 	
 	@GetMapping(produces= {"application/json","application/xml"},consumes= {"application/json","application/xml"})
+	@PreAuthorize("hasAuthority('admin')")
 	public ResponseEntity<List<FlatBooking>> getAllFlatBooking(){
 		return new ResponseEntity<>(flatbookingService.viewAllFlatBookingService(),HttpStatus.OK);
 	}
 }
-
