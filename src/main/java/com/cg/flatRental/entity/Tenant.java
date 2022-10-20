5be56c51d6bf48077a333372a1c5b4581b351a2e
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-10-21 01:26:30 +0530

    Update Tenant.java

--- src/main/java/com/cg/flatRental/entity/Tenant.java
+++ src/main/java/com/cg/flatRental/entity/Tenant.java
@@ -11,30 +11,19 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.cg.flatRental.secure.User;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name="tenant_info")
 public class Tenant extends User{
 	
-//	@NotEmpty(message = "Tenant Name can't be empty!")
-//	@Size(min = 3, max = 25, message = "Invalid Tenant name. Please enter a vaild Tenant name!")
-//	@Pattern(regexp = "^[a-zA-Z]*$", message = "Accepts only alphabets! Re-enter the name")
 	private String tenantName;
 	
-//	@NotEmpty(message = "Tenant Age can't be empty!")
-//	@Size(min = 1, max = 120, message = "Invalid Age. Please enter a vaild age.")
-//	@Pattern(regexp = "^[1-9]$|^[1-9][0-9]$|^(100)$", message = "Invalid input:Enter numbers only")
 	private int tenantAge;
 	
-//	@NotEmpty(message = "Phone Number can't be empty!")
-//	@Size(min = 10, max = 10, message = "Invalid Phone Number. Please enter a vaild phone number of 10 digits")
-//	@Pattern(regexp = "^\\d{10}$", message = "Invalid input:Enter numbers only")
 	private long tenantPhoneNumber;
 	
-//	@NotEmpty(message = "Email ID can't be empty!")
-//	@Size(min = 2, max = 30, message = "Invalid Email ID. Please enter a vaild email ID")
-//	@Email(message = "Not a proper Email ID format! Enter again")
 	private String emailId;
 	
 	@OneToMany(mappedBy = "tenant")
