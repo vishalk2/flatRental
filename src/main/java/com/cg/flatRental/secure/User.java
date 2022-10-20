b1571a87d442fd04daa5dc9e4459a9bb42d76dcb
preetham1243 <72698235+preetham1243@users.noreply.github.com>
2022-10-20 12:44:57 +0530

    Add files via upload

---
+++ src/main/java/com/cg/flatRental/secure/User.java
@@ -0,0 +1,50 @@
+package com.cg.flatRental.secure;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="user_info")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User {
+
+	@Id
+	@GeneratedValue
+	private long userId;
+	private String userName;
+	@JsonIgnore
+	private String password;
+	private String userType;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	
+	
+}
---
+++ src/main/java/com/cg/flatRental/secure/UserDto.java
@@ -0,0 +1,57 @@
+package com.cg.flatRental.secure;
+
+public class UserDto {
+	private String userName;
+    private String password;
+    private String userType;
+    private String name;
+    private long userPhoneNumber;
+    private int userAge;
+    private String emailId;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType.toLowerCase();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getUserPhoneNumber() {
+		return userPhoneNumber;
+	}
+	public void setUserPhoneNumber(long userPhoneNumber) {
+		this.userPhoneNumber = userPhoneNumber;
+	}
+	public int getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+    
+    
+	
+}
