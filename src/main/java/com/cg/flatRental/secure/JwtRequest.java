d9d46e18c411252d1ecbbb40245e8a53957b8c2a
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-11-09 10:12:28 +0530

    Update JwtRequest.java

--- src/main/java/com/cg/flatRental/secure/JwtRequest.java
+++ src/main/java/com/cg/flatRental/secure/JwtRequest.java
@@ -2,23 +2,37 @@ package com.cg.flatRental.secure;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotBlank;
+
 public class JwtRequest implements Serializable {
 
 	private static final long serialVersionUID = 5926468583005150707L;
-	
+	@NotBlank(message = "User Name cannot be empty")
 	private String userName;
+	@NotBlank(message = "Password cannot be empty")
 	private String password;
 	
 	//default constructor for JSON Parsing
 	public JwtRequest()
 	{
 	}
+	
+	
 
+	public JwtRequest(@NotBlank(message = "User Name cannot be empty") String userName,
+			@NotBlank(message = "Password cannot be empty") String password) {
+		super();
+		this.userName = userName;
+		this.password = password;
+	}
+
+
+/*
 	public JwtRequest(String userName, String password) {
 		this.setUserName(userName);
 		this.setPassword(password);
 	}
-
+*/
 	public String getUserName() {
 		return this.userName;
 	}
