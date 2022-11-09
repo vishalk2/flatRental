2b80f39ec6c98ef1770235ad755cdb3f59c4bdee
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-11-09 10:03:50 +0530

    Update LandLord.java

--- src/main/java/com/cg/flatRental/entity/LandLord.java
+++ src/main/java/com/cg/flatRental/entity/LandLord.java
@@ -7,6 +7,9 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -18,8 +21,11 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Table(name="landlord_info")
 public class LandLord extends User{
 	
+	@NotBlank(message = "Landlord Name cannot be empty")
 	private String landLordName;
 	
+	@Min(value = 18, message = "Age must be greater than 18 years!")
+	@Max(value = 120, message = "Age must not be greater than 120 years!")
 	private int landLordAge;
 	
 	@OneToMany(mappedBy = "societyLandlord")
@@ -32,12 +38,29 @@ public class LandLord extends User{
 	
 	private long landLordPhoneNumber;
 	
+	@NotBlank(message = "EmailId cannot be empty")
+	@Email(message = "Not the proper Email ID format! enter again")
 	private String landLordEmailId;
 	
 	@OneToMany
 	private List<BookingRequest> bookingReqs;
 	
 	public LandLord() {
+	}
+
+	public LandLord(@NotBlank(message = "Landlord Name cannot be empty") String landLordName,
+			@Min(value = 18, message = "Age must be greater than 18 years!") @Max(value = 120, message = "Age must not be greater than 120 years!") int landLordAge,
+			List<Society> societyList, List<Flat> flatList, long landLordPhoneNumber,
+			@NotBlank(message = "EmailId cannot be empty") @Email(message = "Not the proper Email ID format! enter again") String landLordEmailId,
+			List<BookingRequest> bookingReqs) {
+		super();
+		this.landLordName = landLordName;
+		this.landLordAge = landLordAge;
+		this.societyList = societyList;
+		this.flatList = flatList;
+		this.landLordPhoneNumber = landLordPhoneNumber;
+		this.landLordEmailId = landLordEmailId;
+		this.bookingReqs = bookingReqs;
 	}
 
 	public String getLandLordName() {
