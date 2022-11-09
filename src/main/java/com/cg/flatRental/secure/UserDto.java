660caf6badb48bd7f95b43e9631e263de5ba1495
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-11-09 10:13:08 +0530

    Update UserDto.java

--- src/main/java/com/cg/flatRental/secure/UserDto.java
+++ src/main/java/com/cg/flatRental/secure/UserDto.java
@@ -1,13 +1,31 @@
 package com.cg.flatRental.secure;
 
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class UserDto {
+	@NotBlank(message = "User Name cannot be empty")
+	@Column(unique = true)
 	private String userName;
+	@NotBlank(message = "Password cannot be empty")
     private String password;
+	@NotBlank(message = "User Type cannot be empty")
     private String userType;
+	@NotBlank(message = "Name cannot be empty")
     private String name;
     private long userPhoneNumber;
+    @Min(value = 18, message = "Age must be greater than 18 years!")
+	@Max(value = 120, message = "Age must not be greater than 120 years!")
     private int userAge;
+    @NotBlank(message = "EmailId cannot be empty")
+	@Email(message = "Not a valid Email ID! Enter again!")
     private String emailId;
+    
 	public String getUserName() {
 		return userName;
 	}
@@ -50,8 +68,4 @@ public class UserDto {
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
-	
-    
-    
-	
 }
