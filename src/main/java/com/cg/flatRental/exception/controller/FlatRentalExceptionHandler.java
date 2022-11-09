625e87b8a4e3284e9704f562066c4cdebf2fda84
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-11-09 10:05:49 +0530

    Update FlatRentalExceptionHandler.java

--- src/main/java/com/cg/flatRental/exception/controller/FlatRentalExceptionHandler.java
+++ src/main/java/com/cg/flatRental/exception/controller/FlatRentalExceptionHandler.java
@@ -1,9 +1,16 @@
 package com.cg.flatRental.exception.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.core.NestedExceptionUtils;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import com.cg.flatRental.dto.APIError;
 import com.cg.flatRental.exceptions.BookingRequestNotFoundException;
@@ -15,6 +22,29 @@ import com.cg.flatRental.exceptions.UserNotFoundException;
 
 @RestControllerAdvice
 public class FlatRentalExceptionHandler {
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleMethodArgsNotValidException(MethodArgumentNotValidException e){
+		Map<String, String> resp = new HashMap<>();
+		e.getBindingResult().getAllErrors().forEach((error)->{
+			String fieldName = ((FieldError) error).getField();
+			String message = error.getDefaultMessage();
+			resp.put(fieldName, message);
+		});
+		return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<?> handleDataIntegrityViolationException(DataIntegrityViolationException e){
+		String msg = NestedExceptionUtils.getMostSpecificCause(e).getMessage();
+		if(msg.contains("user_name")) {
+			APIError error = new APIError("Username already exists. Try again!",409);
+			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+		}
+		APIError error = new APIError("Credentials already exist. Try again!",409);
+		return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+	}
+	
 	@ExceptionHandler(BookingRequestNotFoundException.class)
 	public ResponseEntity<APIError> handleBookingRequests(Exception e){
 		APIError error = new APIError("Booking Requests not found", 404);
