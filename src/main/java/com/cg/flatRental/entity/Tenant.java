3936ae53e1b760e0236969cb6bfbedafc8adbf71
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-11-09 10:05:26 +0530

    Update Tenant.java

--- src/main/java/com/cg/flatRental/entity/Tenant.java
+++ src/main/java/com/cg/flatRental/entity/Tenant.java
@@ -7,6 +7,9 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -18,12 +21,17 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Table(name="tenant_info")
 public class Tenant extends User{
 	
+	@NotBlank(message = "Landlord Name cannot be empty")
 	private String tenantName;
 	
+	@Min(value = 18, message = "Age must be greater than 18 years!")
+	@Max(value = 120, message = "Age must not be greater than 120 years!")
 	private int tenantAge;
 	
 	private long tenantPhoneNumber;
 	
+	@NotBlank(message = "EmailId cannot be empty")
+	@Email(message = "Not the proper Email ID format! enter again")
 	private String emailId;
 	
 	@OneToMany(mappedBy = "tenant")
@@ -32,13 +40,18 @@ public class Tenant extends User{
 	
 	public Tenant() {
 	}
-	
-	public Tenant(String tenantName, int tenantAge, long tenantPhoneNumber, String emailId) {
+
+	public Tenant(@NotBlank(message = "Landlord Name cannot be empty") String tenantName,
+			@Min(value = 18, message = "Age must be greater than 18 years!") @Max(value = 120, message = "Age must not be greater than 120 years!") int tenantAge,
+			long tenantPhoneNumber,
+			@NotBlank(message = "EmailId cannot be empty") @Email(message = "Not the proper Email ID format! enter again") String emailId,
+			List<BookingRequest> bookingReqs) {
 		super();
 		this.tenantName = tenantName;
 		this.tenantAge = tenantAge;
 		this.tenantPhoneNumber = tenantPhoneNumber;
 		this.emailId = emailId;
+		this.bookingReqs = bookingReqs;
 	}
 
 	public String getTenantName() {
