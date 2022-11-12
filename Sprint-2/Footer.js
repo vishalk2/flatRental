4c75b7b28358556d00ff61e234669d6e34d996c7
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-11-12 11:21:48 +0530

    Update Footer.js

--- Sprint-2/Footer.js
+++ Sprint-2/Footer.js
@@ -1,97 +1,100 @@
 import React from "react";
 import "./Footer.css";
-import { FaDiscord, FaInstagram, FaYoutube ,FaPhoneAlt,} from "react-icons/fa";
-import { AiFillMail } from "react-icons/ai";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AiFillMail, AiFillPhone } from "react-icons/ai";
+import { FaFacebook, FaInstagram, FaYoutube ,FaTwitter } from "react-icons/fa";
 
-
-
-
-import { NavLink } from "react-router-dom";
-
-function Footer  () {
+function Footer() {
   return (
-    <div className="Entire-footer">
-    
-     
-      <section className="contact-short">
-       
-        <div className="grid grid-two-column">
-          <div>
-            <hr/>
-            <h3>Ready to get started?</h3>
-            <h3>Talk to us today</h3>
-          </div>
+    <div class="">
+      <footer class="text-white text-center text-lg-start bg-dark">
+        <div class="container p-2">
+          <div class="row mt-2">
+            <div class="col-lg-4 col-md-8 mb-3 mb-md-0">
+              <h5 class="text-uppercase mb-2">About RESERVA</h5>
 
-          
-        </div>
-      </section>
+              <p id="p-footer">
+                Reserva is an online flat rental application which acts as a
+                third party tool connecting Tenants and Landlords.
+              </p>
 
-      {/* footer section  */}
+              <p id="p-footer">
+                This application is built upon Java, Springboot REST and
+                ReactJS.
+              </p>
 
-      <footer>
-        <div className="container grid grid-four-column">
-          <div className="footer-about">
-            <h3>Reserva</h3>
-            <p>Where we care about your Dreams</p>
-          </div>
-
-          {/* 3rs column  */}
-          <div className="footer-social">
-            <h3>Follows Us</h3>
-            <div className="footer-social--icons">
-              <div>
-                <FaDiscord className="icons" />
-              </div>
-              <div>
-                <FaInstagram className="icons" />
-              </div>
-              <div>
-                <FaPhoneAlt className="icons"/>
-              </div>
-              <AiFillMail className="icons"/>
-              <div>
-
-              </div>
-              <div>
+              <div class="mt-2 footer-social--icons">
                 <a
-                  href="https://www.youtube.com"
-                  target="_blank">
+                  type="button"
+                  href="https://www.facebook.com/"
+                >
+                  <FaFacebook className="icons" />
+                </a>
+                <a
+                  type="button"
+                  href="https://www.instagram.com/"
+                >
+                  <FaInstagram className="icons" />
+                </a>
+                <a
+                  type="button"
+                  href="https://www.twitter.com/"
+                >
+                  <FaTwitter className="icons" />
+                </a>
+                <a
+                  type="button"
+                  href="https://www.youtube.com/"
+                >
                   <FaYoutube className="icons" />
                 </a>
               </div>
             </div>
-          </div>
 
+            <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+              <h5 class="text-uppercase mb-4" id="div-contact">Contact</h5>
+              <div id="div-contact">
+                <span class="fa-li">
+                  <AiFillMail className="icons" />
+                </span>
+                <span class="ms-2">contact@reserva.com</span>
+              </div>
+              <div id="div-contact">
+                <span class="fa-li">
+                <AiFillPhone className="icons" />
+                </span>
+                <span class="ms-2">+040 234 567 89</span>
+              </div>
+            </div>
 
-          
+            <div class="col-lg-4 col-md-7 mb-4 mb-md-0">
+              <h5 class="text-uppercase mb-3">Operating hours</h5>
 
-          {/* 4th column  */}
-          <div className="footer-contact">
-            <h3>Call Us</h3>
-            <h3>+91 12345678978</h3>
-          </div>
-        </div>
-
-        {/* bottom section  */}
-        <div className="terms">
-          
-          <div className="container grid grid-two-column">
-            <p>
-              @{new Date().getFullYear()} Reserva. All Rights Reserved
-            </p>
-            <div>
-              <p>PRIVACY POLICY</p>
-              <p>TERMS & CONDITIONS</p>
+              <table class="table text-center text-white">
+                <tbody class="fw-normal">
+                  <tr>
+                    <td>Mon - Fri:</td>
+                    <td>9am - 6pm</td>
+                  </tr>
+                  <tr>
+                    <td>Sat - Sun</td>
+                    <td>10am - 10pm</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-    
+
+        <div
+          class="text-center p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2022 Copyright | RESERVA
+        </div>
       </footer>
-      
     </div>
   );
-};
-
-
+}
 
 export default Footer;
