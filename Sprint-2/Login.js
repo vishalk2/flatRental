87c60a9fe24c8754d1b927d07566e1aa2125ab93
K Badari Vishal <68963724+vishalk2@users.noreply.github.com>
2022-11-13 12:02:33 +0530

    Update Login.js

--- Sprint-2/Login.js
+++ Sprint-2/Login.js
@@ -1,84 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import UserService from "./../Service/UserService";
+import JwtRequest from "./../Model/JwtRequest";
 
-import './css/Login.css'
-
-function Login(props) {
-
-    const initialValues={username:"", password:""};
-    const [formValues, setFormValues] = useState(initialValues);
-    const[formErrors, setFormErrors] = useState({});
-    
-
-    const handleChange = (event) =>{
-        const {name, value} = event.target;
-        setFormValues({...formValues, [name]: value});
-        console.log(JSON.stringify(formValues));
-    }
-    
-
-    const handleSubmit=(event)=>{
-        event.preventDefault();
-        setFormErrors(validateForm(formValues));
-
-    }
-
-    const validateForm = (values) => {
-        const errors = {};
-        // const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-        if(!values.username){
-            errors.username = "Username is required";
-        }
-        if(!values.password){
-            errors.password = "Password is required";
-        }
-        else if(values.password.length<8){
-            errors.password="Password should be atleast 8 characters";
-            {console.log(values.password.length)}
-        }
-        else if (values.password.length>12){
-            errors.password="Password should not be long than 12 Characters";
-            {console.log(values.password.length)}
-
-        }
-        return errors;
-    }
-
-    return (
-        <>
-        <center>
-        <Grid container> 
-            <Grid item   md={6} className="d-none d-md-block">
-                <Box>
-                    
-                </Box>
-            </Grid>
-            <Grid item md={6}>
-        <div id="a">
-            <form id="login-form" onSubmit={handleSubmit}>
-                <h1>Login</h1>
-                <div className="ui divider"></div>
-                <div className="ui form">
-                    <div className="field">
-                        <label>Username</label>
-                        <input type="text" name="username" placeholder='Username' onChange={handleChange}/>
-                    </div>
-                    <p id="error-message">{formErrors.username}</p>
-                    <div className="field">
-                        <label>Password</label>
-                        <input type="password" name="password" placeholder='Password' onChange={handleChange}/>
-                         
-                    </div>
-                    <p id="error-message">{formErrors.password}</p>
-                    <button className='fluid ui button' id="btn">Submit</button>
-                </div>
-                <p style={{paddingTop:'10px'}}>Dont have an account ? <a href="/register" style={{textDecoration:'none'}}>register</a></p>
-            </form>
+function Login() {
+  const [user, setUser] = useState({ login: new JwtRequest() });
+  const navigate = useNavigate();
+  let service = new UserService();
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form>
+        <div className="form-group">
+          <label>Username</label>
+          <input
+            className="form-control"
+            type="text"
+            id="username"
+            placeholder="Enter User Name"
+            value={user.login.userName}
+            onChange={(e) =>
+              setUser({ login: { ...user.login, userName: e.target.value } })
+            }
+          />
         </div>
-        </Grid>
-        </Grid>
-        </center>
-        </>
-    );
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            className="form-control"
+            type="password"
+            id="password"
+            placeholder="Enter Password"
+            value={user.login.password}
+            onChange={(e) =>
+              setUser({ login: { ...user.login, password: e.target.value } })
+            }
+          />
+        </div>
+        <div>
+          <button
+            className="btn btn-outline-primary"
+            onClick={(e) => {
+              e.preventDefault();
+              service
+                .login(user.login)
+                .then((result) => {
+                  alert(JSON.stringify(result));
+                  sessionStorage.setItem("currentUser", JSON.stringify(result));
+                  sessionStorage.setItem("username", user.login.userName);
+                  navigate("/home/ll");
+                })
+                .catch((error) => {
+                  //alert(error.message);
+                  alert("Invalid Username/Password. Please try again!");
+                });
+            }}
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
-
 export default Login;
